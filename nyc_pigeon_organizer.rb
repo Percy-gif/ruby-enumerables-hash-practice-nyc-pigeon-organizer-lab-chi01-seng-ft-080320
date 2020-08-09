dde54e22d48094230c273236d9b525926a1afd54
Percy Major <chipercy@hotmail.com>
2020-08-09 19:48:56 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,6 +6,7 @@ def nyc_pigeon_organizer(data)
     #binding.pry
     value.each do |inner_key, names| #these names are made up
     names.each do |name|
+      if !
         binding.pry
       end 
     end 
