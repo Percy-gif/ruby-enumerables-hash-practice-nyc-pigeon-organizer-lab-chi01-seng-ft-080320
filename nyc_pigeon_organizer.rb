25792f27efb2441fd67df4a10aad9aa3fb212ecd
Percy Major <chipercy@hotmail.com>
2020-08-09 19:06:03 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,10 +2,12 @@ require 'pry'
 
 def nyc_pigeon_organizer(data)
   
-data.each_with_object({}) do |(key, value), final_array|
+  data.each_with_object({}) do |(key, value), final_array|
+
 binding.pry 
 final_array
 end
-
+  
+  binding.pry
  
 end
