e7fe26a2d2e694262b51992d169d3d792daf7c58
Percy Major <chipercy@hotmail.com>
2020-08-09 19:56:39 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -9,7 +9,7 @@ def nyc_pigeon_organizer(data)
         binding.pry
          if !final_array[name]
            final_array[name] = {}
-      end 
+         end 
       if !final_array[name][key] = {}
     end 
     final_array
