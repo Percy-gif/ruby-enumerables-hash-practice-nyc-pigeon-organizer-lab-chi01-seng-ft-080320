07dce01babfa0a365c244c4abb7c9e216ae68345
Percy Major <chipercy@hotmail.com>
2020-08-09 19:00:49 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,25 +2,9 @@ require 'pry'
 
 def nyc_pigeon_organizer(data)
   
+hash.each_with_object({}) do |(k, v), a|
+ a[k] = v + 3
+end
   binding.pry
-  # write your code here!
-  
-# pigeon_data = {
-#   :color => {
-#     :purple => ["Theo", "Peter Jr.", "Lucky"],
-#     :grey => ["Theo", "Peter Jr.", "Ms. K"],
-#     :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
-#     :brown => ["Queenie", "Alex"]
-#   },
-#   :gender => {
-#     :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
-#     :female => ["Queenie", "Ms. K"]
-#   },
-#   :lives => {
-#     "Subway" => ["Theo", "Queenie"],
-#     "Central Park" => ["Alex", "Ms. K", "Lucky"],
-#     "Library" => ["Peter Jr."],
-#     "City Hall" => ["Andrew"]
-#   }
-# }
+ 
 end
